f11e531e672e1c63d09d410b2ce4829059a65f5b
Ariel Martin <arielmartin80@gmail.com>
2022-03-31 15:08:32 -0300

    se automatizó About

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const datos = require('../../package.json')
 
 router.get('/', (req, res) => {
-	res.render('index');
+    res.render('index', { datos });
 });
 
 router.get('/about', (req, res) => {
-	res.render('about');
+    res.render('about');
 });
 
 
