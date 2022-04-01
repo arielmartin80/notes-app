31d87595e13efe50d80337be89d52d44c1f00509
Ariel Martin <arielmartin80@gmail.com>
2022-04-01 19:55:44 -0300

    se agregó config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const username = 'userTest'
+const password = 'passTest'
+
+const config = {
+    USERNAME: username,
+    PASSWORD: password,
+    ATLAS: username + ':' + password
+}
+
+module.exports = config
