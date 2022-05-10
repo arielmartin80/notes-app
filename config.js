a0a0121aba648edc40c0a973d0a6ab569f325729
Ariel Martin <arielmartin80@gmail.com>
2022-05-10 20:48:56 -0300

    variables de entorno

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-const username = 'userTest'
-const password = 'passTest'
-
 const config = {
-    USERNAME: username,
-    PASSWORD: password,
-    ATLAS: username + ':' + password
-}
+    USERNAME: process.env.DB_USERNAME,
+    PASSWORD: process.env.DB_PASSWORD,
+    ATLAS: process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD
 
+}
+console.log(config)
 module.exports = config
